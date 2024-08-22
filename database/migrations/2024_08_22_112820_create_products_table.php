41288071f6fb60f920c2fcbb6381efead8bf7935
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2024-08-22 12:31:19 -0400

    add product model

---
+++ database/migrations/2024_08_22_112820_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id')
+                ->primary();
+
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price');
+            $table->integer('stock');
+            $table->string('image')
+                ->nullable();
+            $table->boolean('is_active')
+                ->default(true);
+            $table->string('currency')
+                ->default('CUP');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
