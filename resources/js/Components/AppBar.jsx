d9ac07d8d9329f1e57656fec3afdd3ebb5bcd64a
Guillermo Perez <luisguillermo.rodriguez32@gmail.com>
2024-08-30 07:50:56 -0400

    add logo

--- resources/js/Components/AppBar.jsx
+++ resources/js/Components/AppBar.jsx
@@ -6,7 +6,7 @@ const AppBar = () => {
     return (
         <div className="p-4 w-full rounded-b-lg bg-white flex items-center">
             <Link href={route("home")}>
-                <div className="h-10 w-10 bg-[#FF6200]"></div>
+                <img src="/logo.png" alt="logo timbirichi" />
             </Link>
 
             <Link href={route("home")} className="ml-auto">
